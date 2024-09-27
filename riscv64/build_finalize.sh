2fb75b775d1243ac945e9546913e04108f4bca47
Ruoqing He <heruoqing@iscas.ac.cn>
2024-09-27 10:10:27 +0000

    riscv64: Replace `9p` rootfs  with `virtio-blk`
    Move all contents in rootfs previously used in `9p` to a raw image,
    enable VM to boot with normal `virtio-blk` device.
    
    Which addresses failures while running unit-tests of `linux-loader` and
    `vm-memory`.
    
    Signed-off-by: Ruoqing He <heruoqing@iscas.ac.cn>

--- riscv64/build_finalize.sh
+++ riscv64/build_finalize.sh
@@ -5,7 +5,7 @@ apt-get update
 
 DEBIAN_FRONTEND="noninteractive" apt-get install --no-install-recommends -y \
     openssh-client libslirp-dev libfdt-dev libglib2.0-dev libssl-dev \
-    libpixman-1-dev netcat
+    libpixman-1-dev netcat qemu-utils
 
 # Setup container ssh config
 yes "" | ssh-keygen -P ""
@@ -21,3 +21,14 @@ EOF
 # Set `nameserver` to `10.0.2.3` as QEMU User Networking documented.
 # See: https://wiki.qemu.org/Documentation/Networking
 echo 'nameserver 10.0.2.3' > $ROOTFS_DIR/etc/resolv.conf
+
+TMP_MOUNT_DIR=tmp_mount_dir
+# Move rootfs into image with raw format
+qemu-img create $ROOTFS_IMG 10G && mkfs.ext4 $ROOTFS_IMG
+mkdir $TMP_MOUNT_DIR
+# This operation need `privileged` docker container
+mount -o loop $ROOTFS_IMG $TMP_MOUNT_DIR
+mv $ROOTFS_DIR/* $TMP_MOUNT_DIR
+umount $TMP_MOUNT_DIR
+rmdir $TMP_MOUNT_DIR
+
